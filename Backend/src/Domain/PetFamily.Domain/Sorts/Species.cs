4d4dfa0994a823ee3c6909359a90341bf0a98778
Lukashenko Vladimir <lukashenko200440@gmail.com>
2024-08-16 08:46:46 +0300

    Update Species.cs

--- Backend/src/Domain/PetFamily.Domain/Sorts/Species.cs
+++ Backend/src/Domain/PetFamily.Domain/Sorts/Species.cs
@@ -8,24 +8,20 @@ namespace PetFamily.Domain.Sorts;
 public class Species : BaseEntity<SpeciesId>
 {
     public Species(SpeciesId id) : base(id) { }
-    private Species(SpeciesId id, string value, List<Breed> breeds, List<Pet> pets) : base(SpeciesId.NewId())
+    private Species(SpeciesId id, string value, List<Breed> breeds) : base(SpeciesId.NewId())
     {
         Id = id;
         Value = value;
         _breeds = breeds;
-        _pets = pets;
     }
     public SpeciesId Id { get; private set; }
     public string Value { get; private set; }
     public IReadOnlyList<Breed> Breeds => _breeds;
     private readonly List<Breed> _breeds;
-    
-    public IReadOnlyList<Pet> Pets => _pets;
-    private readonly List<Pet> _pets;
 
-    public static Result<Species> Create(SpeciesId id, string value, List<Breed> breeds, List<Pet> pets)
+    public static Result<Species> Create(SpeciesId id, string value, List<Breed> breeds)
     {
-        var species = new Species(id, value, breeds, pets);
+        var species = new Species(id, value, breeds);
         return Result.Success(species);
     }
 }
